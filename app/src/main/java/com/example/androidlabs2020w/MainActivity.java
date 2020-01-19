92d2b33e4f993e5ef7f88fafb83c347312b1710c
owenbailey <owen.bailey99@gmail.com>
2020-01-19 16:20:13 -0500

    Added 3 layouts for Lab 2

--- app/src/main/java/com/example/androidlabs2020w/MainActivity.java
+++ app/src/main/java/com/example/androidlabs2020w/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.androidlabs2020w;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.activity_main_grid);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, getResources().getString(R.string.toast_message), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        CheckBox cb = (CheckBox) findViewById(R.id.checkBox);
+
+        cb.setChecked(true);
+
+
+        Snackbar.make(cb, "Checkbox is " ,Snackbar.LENGTH_LONG)
+
+                .show();
+        }
 }
+
