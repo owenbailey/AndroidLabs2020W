38093a1e1a297711bf1424aacc9e95c666eed536
owenbailey <owen.bailey99@gmail.com>
2020-02-06 14:07:39 -0500

    lab 4

--- app/src/main/java/com/example/androidlabs2020w/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs2020w/ChatRoomActivity.java
@@ -6,10 +6,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -19,27 +21,64 @@ public class ChatRoomActivity extends AppCompatActivity {
 
     ArrayList<Message> MessageObj = new ArrayList<>();
 
-    ListAdapter myAdapter = new ListAdapter() {
-    };
+    myAdapter myAdapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_chat_room);
+
+        ListView lv = (ListView) findViewById(R.id.theListView);
+
+        EditText et = (EditText) findViewById(R.id.textMessageField);
+
+        lv.setAdapter(myAdapter = new myAdapter());
+
+        lv.setOnItemClickListener( (p, b, pos, id) -> {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle("Do you want to delete this?")
+
+                    //What is the message:
+                    .setMessage("The selected row" + pos + "\nThe database id is:" + id)
+
+
+                    //what the Yes button does:
+                    .setPositiveButton("Yes", (click, arg) -> {
+                        MessageObj.remove(pos );
+                        myAdapter.notifyDataSetChanged();
+                    })
+                    //What the No button does:
+                    .setNegativeButton("No", (click, arg) -> { });
+
+                    //An optional third button:
+                    //   .setNeutralButton("Maybe", (click, arg) -> {  })
+
+                    //You can add extra layout elements:
+                    //.setView(getLayoutInflater().inflate(R.layout.row_layout, null) )
+
+                    //Show the dialog
+                    alertDialogBuilder.create().show();
+
+        });
 
         Button sb = (Button) findViewById(R.id.sendButton);
         sb.setOnClickListener(v -> {
-
-            MessageObj.add();
+Message newmsg = new Message(et.getText().toString(), true);
+            MessageObj.add(newmsg);
             myAdapter.notifyDataSetChanged();
-
+            et.setText("");
 
         });
 
         Button rb = (Button) findViewById(R.id.receiveButton);
         rb.setOnClickListener(v -> {
+            Message newmsg = new Message(et.getText().toString(), false);
 
-
+            MessageObj.add(newmsg);
+            myAdapter.notifyDataSetChanged();
+            et.setText("");
         });
 
 
@@ -49,8 +88,29 @@ public class ChatRoomActivity extends AppCompatActivity {
 
     public class Message{
 
-        Boolean isSend;
-//boolean isSend
+
+        String msgtyped;
+
+        public Message(String s, boolean b){
+            msgtyped = s;
+
+            isSend = b;
+
+
+
+        }
+
+        public String getMsgtyped(){
+            return msgtyped;
+        }
+
+        public boolean isSend(){
+            return isSend;
+        }
+
+
+
+        boolean isSend;
     }
 
     private class myAdapter extends BaseAdapter {
@@ -71,10 +131,11 @@ public class ChatRoomActivity extends AppCompatActivity {
             if (thisrow.isSend())
               newView = inflater.inflate(R.layout.layout_send, parent, false);
             else
+                newView = inflater.inflate(R.layout.layout_receive, parent, false);
 
             //set what the text should be for this row:
             TextView tView = newView.findViewById(R.id.textMessageField);
-            tView.setText( getItem(position).toString() );
+            tView.setText( getItem(position).getMsgtyped() );
 
             //return it to be put in the table
             return newView;
